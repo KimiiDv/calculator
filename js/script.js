9ef0bd5547fcafe714661ec521111553135be197
KimiiDv <cristianjosue0707@hotmail.com>
2022-08-27 22:08:55 -0400

    Update js

--- js/script.js
+++ js/script.js
@@ -3,16 +3,28 @@ let result = document.getElementById('results');
 
 for (let i = 0; i < btn.length; i++) {
     btn[i].addEventListener('click', function () {
+        // Si existe un numero lo borramos
+        if (result.textContent == '0') result.textContent = ''
         if (this.innerHTML == '=') {
             result.innerHTML = eval(result.innerHTML)
         } else {
-            //Pressing AC deletes the numbers
+            //Borramos
             if (this.innerHTML == 'AC') {
                 result.innerHTML = '';
             } else { 
-                // Send results
+                // enviamos resultados
                 result.innerHTML += this.innerHTML
+                // si el contenido es mayor que 24 lo borramos
+                maximumAllowed()
             }
         }
     })
+
+}
+
+function maximumAllowed() {
+    if (result.textContent.length >= 24) {
+        // console.log('es mayour que 24')
+        result.textContent = ''
+    }
 }
